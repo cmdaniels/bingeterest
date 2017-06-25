eb885b77523eee2ee72f1ae87e79c4afce4bd3e9
Cody Daniels <codydaniels@gmail.com>
2017-06-24 22:01:07 -0700

    Add ejs templating

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
+  res.render('index', {
+    signedIn: false
+  });
+});
+
+app.get('/recent', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/login', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
