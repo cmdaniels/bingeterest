dc590fd103ddde321a67a374e1de57174f2fc950
Cody Daniels <codydaniels@gmail.com>
2017-06-25 23:31:38 -0700

    Add recent binges page

---
+++ public/js/recent.js
@@ -0,0 +1,17 @@
+firebase.database().ref('posts/').once('value', function(snapshot) {
+  var posts = snapshot.val();
+  for (var id in posts) {
+    if (posts.hasOwnProperty(id)) {
+      var post = posts[id];
+      $('.binges').append('<div class="panel panel-default binge" id="' + id + '">' +
+        '<div class="panel-heading">' +
+          '<h3 class="panel-title">' + post.title + '</h3>' +
+        '</div>' +
+        '<div class="panel-body">' +
+          '<a href="#" class="thumbnail"><img src="' + post.url + '"></a>' +
+          '<a href="/' + post.userId + '">' + post.displayName + '</a>' +
+        '</div>' +
+      '</div>');
+    }
+  }
+});
